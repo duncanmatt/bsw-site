38f92e649ca04a1c5ea3637cabee0fee5a844820
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-02-04 22:23:54 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.status(200).send('HELLO');
-});
+
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/newsletters', require('./routes/newsletterRoutes'));
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+	// set build folder as static
+	app.use(express.static(path.join(__dirname, '.../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'),
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to the Support Desk API' });
+	});
+}
 
 app.use(errorHandler);
 
