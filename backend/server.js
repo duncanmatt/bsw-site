23629cc0a93aa0d36c27fb6fe0d8eae7975d4a84
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-02-05 12:51:38 -0500

    removed procfile

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 	// set build folder as static
 	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-	app.get('/*', (req, res) =>
+	app.get('*', (req, res) =>
 		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'),
 	);
 } else {
